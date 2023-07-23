cf4064ebafb0d7fed9ae6ba83b6d71ff8540a34d
WowliamGuo <64669373+WowliamGuo@users.noreply.github.com>
2023-07-23 00:32:49 -0700

    socket.io

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const config = require('./utils/config')
 const express = require('express')
+const http = require('http')
 const app = express()
 const cors = require('cors')
+const socketIO = require('socket.io')
 const goalsRouter = require('./controllers/goals')
 const usersRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 const tasksRouter = require('./controllers/tasks')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+
+const server = http.createServer(app)
+
 const mongoose = require('mongoose')
-
 mongoose.set('strictQuery', false)
-
 logger.info("connecting to mongoDB")
-
 mongoose.connect(config.MONGODB_URI)
     .then(result => {
         logger.info('connected to mongoDB')
@@ -21,6 +23,13 @@ mongoose.connect(config.MONGODB_URI)
     .catch((error) => {
         logger.info('error connecting to mongoDB')
     })
+
+
+const io = socketIO(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    }
+})
 
 app.use(cors())
 app.use(express.json())
@@ -35,4 +44,14 @@ app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
-module.exports = app
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`)
+
+    socket.on("join_room", (data) => {
+        console.log(`now online`)
+        console.log(data)
+        io.emit("joined_room", data)
+    })
+})
+
+module.exports = server     
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const config = require('./utils/config')
-const app = require('./app')
+const server = require('./app')
 const logger = require('./utils/logger')
 
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
     logger.info(`Server running on ${config.PORT}. Happy working!`)
 })
